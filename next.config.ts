4f812fd4feec5473c2e4bffee2bd7de3fc3e5b53
MiKelSX <139016051+MiKelSX@users.noreply.github.com>
2026-09-07 23:53:11 -0300

    Configure basePath and assetPrefix in next.config.ts
    Update basePath and assetPrefix for deployment.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  unoptimizedImages: true,
-  basePath: "",
-  assetPrefix: "",
+  basePath: "/prueba-glass",   // nombre del repo
+  assetPrefix: "/prueba-glass", // asegura que los assets se sirvan bien
+  images: {
+    unoptimized: true, // esta es la forma correcta
+  },
 };
 
 export default nextConfig;
